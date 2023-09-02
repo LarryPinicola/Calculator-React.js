9acb8bfe112214257c3483844d32e297ecd36600
LarryPinicola <thelarry1549@gmail.com>
2023-09-02 10:13:56 +0630

    initial

---
+++ src/DigitButton.jsx
@@ -0,0 +1,13 @@
+import { ACTIONS } from "./App";
+
+const DigitButton = ({ dispatch, digit }) => {
+  return (
+    <button
+      onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}
+    >
+      {digit}
+    </button>
+  );
+};
+
+export default DigitButton;
---
+++ src/OperationButton.jsx
@@ -0,0 +1,15 @@
+import { ACTIONS } from "./App";
+
+const OperationButton = ({ dispatch, operation }) => {
+  return (
+    <button
+      onClick={() =>
+        dispatch({ type: ACTIONS.CHOOSE_OPERATION, payload: { operation } })
+      }
+    >
+      {operation}
+    </button>
+  );
+};
+
+export default OperationButton;
